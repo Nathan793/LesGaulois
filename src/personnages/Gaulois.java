9e88c9eedf6a4e484274bd8d6cb0a398dc8f3d3a
Nathan793 <nathanseba970@gmail.com>
2026-01-08 11:58:28 +0100

    question6

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,13 +4,21 @@ public class Gaulois {
 	private String nom;
 	private int force;
 
-	Gaulois(String nom, int force) {
+	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
 
-	public String getNom() {
+	private String getNom() {
 		return nom;
 	}
-
+	
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		System.out.println(asterix.getNom());
+	}
+	
+	public parler(String nom, String texte) {
+		
+	}
 }
